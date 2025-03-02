23eee359fa8f237673a3c25239ecc4efa6934c28
WangHaonie <68315280+WangHaonie@users.noreply.github.com>
2025-03-02 09:11:55 +0800

    v3.0.9: ✨ 尝试引入 WPF - 10%

---
+++ PlainCEETimer/WPF/Windows/AboutWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using PlainCEETimer.Modules;
+using PlainCEETimer.WPF.ViewModels;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+
+namespace PlainCEETimer.WPF.Windows
+{
+    public sealed partial class AboutWindow
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+            InitializeReactive();
+        }
+
+        private void InitializeReactive()
+        {
+            ViewModel = new AboutViewModel();
+
+            this.WhenActivated(Disposables =>
+            {
+                this.OneWayBind(ViewModel, VM => VM.Logo, V => V.Icon).DisposeWith(Disposables);
+            });
+        }
+    }
+}
