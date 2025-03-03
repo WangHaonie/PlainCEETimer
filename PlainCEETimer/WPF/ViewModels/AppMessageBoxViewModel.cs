a05df25588b834369d8d99a995bbfc9417ab984d
WangHaonie <68315280+WangHaonie@users.noreply.github.com>
2025-03-03 20:16:06 +0800

    v3.0.9: ✨ WPF 重写 AppMessageBox - 5%

---
+++ PlainCEETimer/WPF/ViewModels/AppMessageBoxViewModel.cs
@@ -0,0 +1,18 @@
+﻿using PlainCEETimer.WPF.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainCEETimer.WPF.ViewModels
+{
+    public class AppMessageBoxViewModel : ViewModelBase
+    {
+        public string WindowTitle
+        {
+            get => field;
+            set => SetField(ref field, value);
+        }
+    }
+}
