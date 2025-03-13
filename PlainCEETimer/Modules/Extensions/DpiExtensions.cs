08cc87d278121d84ec32da2dac81886d8b0ba278
WangHaonie <68315280+WangHaonie@users.noreply.github.com>
2025-03-13 11:34:36 +0800

    v3.0.9: 🛠️ 修复多次获取 DPI

--- PlainCEETimer/Modules/Extensions/DpiExtensions.cs
+++ PlainCEETimer/Modules/Extensions/DpiExtensions.cs
@@ -5,12 +5,17 @@ namespace PlainCEETimer.Modules.Extensions
 {
     public static class DpiExtensions
     {
-        public static double DpiRatio { get; private set; } = 0;
+        public static double DpiRatio { get; private set; } = 0D;
 
         public static int ScaleToDpi(this int px, Control ctrl)
         {
             int pxScaled;
-            DpiRatio = NativeInterop.GetDpiForWindow(ctrl.Handle) / 96D;
+
+            if (DpiRatio == 0D)
+            {
+                DpiRatio = NativeInterop.GetDpiForWindow(ctrl.Handle) / 96D;
+            }
+
             pxScaled = (int)(px * DpiRatio);
             return pxScaled;
         }
