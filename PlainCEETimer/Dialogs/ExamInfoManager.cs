b3d2c284ebf1c3e7cb8f030601d96342fb641441
WangHaonie <w2097454439@gmail.com>
2025-05-20 10:55:06 +0800

    v5.0.2: ✨ 考试信息管理器项目分组

--- PlainCEETimer/Dialogs/ExamInfoManager.cs
+++ PlainCEETimer/Dialogs/ExamInfoManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 using PlainCEETimer.Controls;
 using PlainCEETimer.Modules;
 using PlainCEETimer.Modules.Configuration;
@@ -7,13 +8,44 @@ namespace PlainCEETimer.Dialogs
 {
     public sealed class ExamInfoManager : ListViewDialog<ExamInfoObject, ExamInfoDialog>
     {
-        public ExamInfoManager()
-            : base(440, "管理考试信息 - 高考倒计时", "考试信息", ["考试名称", "开始日期和时间", "结束日期和时间"]) { }
+        private readonly ListViewGroup[] Groups;
+
+        public ExamInfoManager() : base(440, "管理考试信息 - 高考倒计时", "考试信息", ["考试名称", "开始日期和时间", "结束日期和时间"])
+        {
+            SetGroups(Groups =
+            [
+                new("已过去的"),
+                new("正在进行"),
+                new("未开始的")
+            ]);
+        }
 
         protected override ListViewItem GetListViewItem(ExamInfoObject data)
-            => new([data.Name, data.Start.Format(), data.End.Format()]);
+        {
+            return new([data.Name, data.Start.Format(), data.End.Format()], GetGroup(data));
+        }
 
         protected override ISubDialog<ExamInfoObject> GetSubDialog(ExamInfoObject data = null)
-            => new ExamInfoDialog(data);
+        {
+            return new ExamInfoDialog(data);
+        }
+
+        private ListViewGroup GetGroup(ExamInfoObject data)
+        {
+            int i = 1;
+            var Now = DateTime.Now;
+
+            if (Now > data.End)
+            {
+                i = 0;
+            }
+
+            if (Now < data.Start)
+            {
+                i = 2;
+            }
+
+            return Groups[i];
+        }
     }
 }
