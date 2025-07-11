c324ba1eee4bc8aa1717b954a2c07b5f5913bfcc
WangHaonie <w2097454439@gmail.com>
2025-07-11 14:18:44 +0800

    v5.0.4: 🛠️ 修复笔记本电脑上不插电不自启

--- PlainCEETimer/Modules/Win32TaskScheduler.cs
+++ PlainCEETimer/Modules/Win32TaskScheduler.cs
@@ -19,11 +19,33 @@ namespace PlainCEETimer.Modules
             IsTaskSchd = CheckStartUpState() == 0;
         }
 
+        public static void SetStartUpTask()
+        {
+            if (CheckStartUpState() is 1 or 2)
+            {
+                var xml = $"{AppPath}.Task.xml";
+
+                File.WriteAllText(xml, $@"<?xml version=""1.0"" encoding=""UTF-16""?><Task version=""1.2"" xmlns=""http://schemas.microsoft.com/windows/2004/02/mit/task""><RegistrationInfo><Author>WangHaonie</Author><Description>用于在 {UserNameOnly} 登录时自动运行</Description></RegistrationInfo><Triggers><LogonTrigger><Enabled>true</Enabled><UserId>{UserName}</UserId></LogonTrigger></Triggers><Principals><Principal id=""Author""><UserId>{UserName}</UserId><LogonType>InteractiveToken</LogonType><RunLevel>LeastPrivilege</RunLevel></Principal></Principals><Settings><DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries><StopIfGoingOnBatteries>false</StopIfGoingOnBatteries></Settings><Actions Context=""Author""><Exec><Command>""{AppPath}""</Command></Exec></Actions></Task>", Encoding.Unicode);
+                ProcessHelper.Run("schtasks", $"/create /tn {TaskName} /xml \"{xml}\" /f", getExitCode: true);
+                File.Delete(xml);
+                RefreshStartUpState();
+            }
+        }
+
+        public static void DeleteStartUpTask()
+        {
+            if (CheckStartUpState() != 3)
+            {
+                ProcessHelper.Run("schtasks", $"/delete /tn {TaskName} /f", getExitCode: true);
+                RefreshStartUpState();
+            }
+        }
+
         /// <summary>
         /// 获取本程序的自启动状态。
         /// </summary>
         /// <returns>0 - 已设置，1 - 设置异常，2 - 命令出错，3 - 环境异常</returns>
-        public static int CheckStartUpState()
+        private static int CheckStartUpState()
         {
             if (Win32User.NotElevated)
             {
@@ -42,7 +64,9 @@ namespace PlainCEETimer.Modules
                         var xml = Xml.FormString(raw);
 
                         if (xml.Check("true", true, "Triggers", "LogonTrigger", "Enabled") &&
-                            xml.Check($"\"{AppPath}\"", false, "Actions", "Exec", "Command"))
+                            xml.Check($"\"{AppPath}\"", false, "Actions", "Exec", "Command") &&
+                            xml.Check("false", false, "Settings", "DisallowStartIfOnBatteries") &&
+                            xml.Check("false", false, "Settings", "StopIfGoingOnBatteries"))
                         {
                             return 0;
                         }
@@ -57,48 +81,6 @@ namespace PlainCEETimer.Modules
             }
 
             return 3;
-        }
-
-        public static void SetStartUpTask()
-        {
-            if (CheckStartUpState() is 1 or 2)
-            {
-                var xml = $"{AppPath}.Task.xml";
-                var args = $"/create /tn {TaskName} /xml \"{xml}\" /f";
-
-                File.WriteAllText(xml, $@"<?xml version=""1.0"" encoding=""UTF-16""?><Task version=""1.2"" xmlns=""http://schemas.microsoft.com/windows/2004/02/mit/task""><RegistrationInfo><Author>WangHaonie</Author><Description>用于在 {UserNameOnly} 登录时自动运行</Description></RegistrationInfo><Triggers><LogonTrigger><Enabled>true</Enabled><UserId>{UserName}</UserId></LogonTrigger></Triggers><Principals><Principal id=""Author""><UserId>{UserName}</UserId><LogonType>InteractiveToken</LogonType><RunLevel>LeastPrivilege</RunLevel></Principal></Principals><Actions Context=""Author""><Exec><Command>""{AppPath}""</Command></Exec></Actions></Task>", Encoding.Unicode);
-
-                if (IsWin7)
-                {
-                    ProcessHelper.Run("cmd", $"/c chcp 437 >nul && schtasks " + args, getExitCode: true);
-                }
-                else
-                {
-                    ProcessHelper.Run("schtasks", args, getExitCode: true);
-                }
-
-                File.Delete(xml);
-                RefreshStartUpState();
-            }
-        }
-
-        public static void DeleteStartUpTask()
-        {
-            if (CheckStartUpState() != 3)
-            {
-                var args = $"/delete /tn {TaskName} /f";
-
-                if (IsWin7)
-                {
-                    ProcessHelper.Run("cmd", $"/c chcp 437 >nul && schtasks ", getExitCode: true);
-                }
-                else
-                {
-                    ProcessHelper.Run("schtasks", args, getExitCode: true);
-                }
-
-                RefreshStartUpState();
-            }
         }
     }
 }
