396c88beb7f08155d030b908b34963ebb7db4e03
WangHaonie <w2097454439@gmail.com>
2025-09-07 07:48:04 +0800

    v5.0.8: 🛠️ 解耦倒计时主要代码 - 失败
    屎山就是不一样，动了就无了。以后有精力了再搞

--- src/PlainCEETimer/UI/Dialogs/ExamInfoManager.cs
+++ src/PlainCEETimer/UI/Dialogs/ExamInfoManager.cs
@@ -8,6 +8,8 @@ namespace PlainCEETimer.UI.Dialogs;
 
 public sealed class ExamInfoManager : ListViewDialog<ExamInfoObject, ExamInfoDialog>
 {
+    private DateTime Now;
+
     public ExamInfoManager()
         : base(450, ["考试名称", "开始日期和时间", "结束日期和时间"], ["已过去的", "正在进行", "未开始的"])
     {
@@ -17,14 +19,14 @@ public sealed class ExamInfoManager : ListViewDialog<ExamInfoObject, ExamInfoDia
 
     protected override int GetGroupIndex(ExamInfoObject data)
     {
-        var now = DateTime.Now;
+        Now = DateTime.Now;
 
-        if (now > data.End)
+        if (Now > data.End)
         {
             return 0;
         }
 
-        if (now < data.Start)
+        if (Now < data.Start)
         {
             return 2;
         }
