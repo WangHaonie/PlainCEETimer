86b4e8149823e8661a95c901e3009f45e6f76954
WangHaonie <w2097454439@gmail.com>
2026-01-10 11:27:43 +0800

    v5.0.9: 🛠️ 整理代码

--- src/PlainCEETimer/Modules/AppInfo.cs
+++ src/PlainCEETimer/Modules/AppInfo.cs
@@ -2,7 +2,7 @@
 
 internal static class AppInfo
 {
-    public const string Version = "0.0.0";
+    public const string Version = "9999.9";
     public const string CommitSHA = "0a1b2c3";
     public const string BuildDate = "9999/12/31";
 }
