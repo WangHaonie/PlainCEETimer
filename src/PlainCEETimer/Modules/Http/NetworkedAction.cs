80ca9a06853fbd37acca9a81106aba93dd62cfa4
WangHaonie <68315280+WangHaonie@users.noreply.github.com>
2026-05-01 17:12:32 +0800

    v5.0.12: 🛠️ 完善网络连通性检查

--- src/PlainCEETimer/Modules/Http/NetworkedAction.cs
+++ src/PlainCEETimer/Modules/Http/NetworkedAction.cs
@@ -11,6 +11,13 @@ public class NetworkedAction(Action action)
 {
     private int m_flag;
 
+    private static readonly string[] m_tests =
+    [
+        "http://g.cn/generate_204",
+        "http://connectivitycheck.gstatic.com/generate_204",
+        "http://connect.rom.miui.com/generate_204"
+    ];
+
     public void Invoke()
     {
         if (action != null)
@@ -52,16 +59,29 @@ public class NetworkedAction(Action action)
 
     private static async Task<bool> CheckConnectivityAsync()
     {
-        try
-        {
-            using var req = new HttpRequestMessage(HttpMethod.Head, "http://g.cn/generate_204");
-            using var res = await HttpService.SendAsync(req);
-
-            return res.IsSuccessStatusCode || res.StatusCode == HttpStatusCode.NoContent;
-        }
-        catch
+        if (!NetworkInterface.GetIsNetworkAvailable())
         {
             return false;
         }
+
+        foreach (var url in m_tests)
+        {
+            try
+            {
+                using var req = new HttpRequestMessage(HttpMethod.Head, url);
+                using var res = await HttpService.SendAsync(req);
+
+                if (res.StatusCode == HttpStatusCode.NoContent)
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                continue;
+            }
+        }
+
+        return false;
     }
 }
